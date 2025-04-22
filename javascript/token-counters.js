54fac60bf51124660067cbbe26c954a4c4526de1
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2025-04-22 23:10:15 +0100

    Add files via upload

--- javascript/token-counters.js
+++ javascript/token-counters.js
@@ -1,51 +1,10 @@
 let promptTokenCountUpdateFunctions = {};
 
-function update_txt2img_tokens(...args) {
-    // Called from Gradio
-    update_token_counter("txt2img_token_button");
-    update_token_counter("txt2img_negative_token_button");
-    if (args.length == 2) {
-        return args[0];
-    }
-    return args;
-}
-
-function update_img2img_tokens(...args) {
-    // Called from Gradio
-    update_token_counter("img2img_token_button");
-    update_token_counter("img2img_negative_token_button");
-    if (args.length == 2) {
-        return args[0];
-    }
-    return args;
-}
-
-function update_token_counter(button_id) {
-    promptTokenCountUpdateFunctions[button_id]?.();
-}
-
-
-function recalculatePromptTokens(name) {
-    promptTokenCountUpdateFunctions[name]?.();
-}
-
-function recalculate_prompts_txt2img() {
-    // Called from Gradio
-    recalculatePromptTokens('txt2img_prompt');
-    recalculatePromptTokens('txt2img_neg_prompt');
-    return Array.from(arguments);
-}
-
-function recalculate_prompts_img2img() {
-    // Called from Gradio
-    recalculatePromptTokens('img2img_prompt');
-    recalculatePromptTokens('img2img_neg_prompt');
-    return Array.from(arguments);
-}
 
 function setupTokenCounting(id, id_counter, id_button) {
     var prompt = gradioApp().getElementById(id);
     var counter = gradioApp().getElementById(id_counter);
+    var button = gradioApp().getElementById(id_button);
     var textarea = gradioApp().querySelector(`#${id} > label > textarea`);
 
     if (counter.parentElement == prompt.parentElement) {
@@ -57,7 +16,7 @@ function setupTokenCounting(id, id_counter, id_button) {
 
     var func = onEdit(id, textarea, 800, function() {
         if (counter.classList.contains("token-counter-visible")) {
-            gradioApp().getElementById(id_button)?.click();
+            button?.click();
         }
     });
     promptTokenCountUpdateFunctions[id] = func;
