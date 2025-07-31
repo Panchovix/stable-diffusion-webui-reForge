1971ab57de726da20908d3cd689930bf8442ebf1
panchovix <francisco.jaqueat@gmail.com>
2025-07-31 00:52:30 -0400

    Fix XYZ grid with Pillow >=10.0.0 (for python 3.12)

--- modules/images.py
+++ modules/images.py
@@ -168,9 +168,19 @@ def draw_grid_annotations(im, width, height, hor_texts, ver_texts, margin=0):
         for line in lines:
             fnt = initial_fnt
             fontsize = initial_fontsize
-            while drawing.multiline_textsize(line.text, font=fnt)[0] > line.allowed_width and fontsize > 0:
-                fontsize -= 1
-                fnt = get_font(fontsize)
+            if parse_version(pillow_version) >= parse_version('10.0.0'):
+                # New code for Pillow 10.0.0+
+                text_width, text_height = drawing.multiline_textbbox((0, 0), line.text, font=fnt)[2:]
+                while text_width > line.allowed_width and fontsize > 0:
+                    fontsize -= 1
+                    fnt = get_font(fontsize)
+                    text_width, text_height = drawing.multiline_textbbox((0, 0), line.text, font=fnt)[2:]
+            else:
+                # Old code for Pillow versions below 10.0.0
+                while drawing.multiline_textsize(line.text, font=fnt)[0] > line.allowed_width and fontsize > 0:
+                    fontsize -= 1
+                    fnt = get_font(fontsize)
+
             drawing.multiline_text((draw_x, draw_y + line.size[1] / 2), line.text, font=fnt, fill=color_active if line.is_active else color_inactive, anchor="mm", align="center")
 
             if not line.is_active:
